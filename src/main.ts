a328c7bed727631ea2a52ce2e3ce122dd1057bda
Amy Hsieh 謝淑雅 Leadtek <amy_hsieh@leadtek.com.tw>
2025-08-13 15:35:29 +0800

    feat(theme): setup vuetify with global styles and validation libraries

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import { vuetifyConfig } from './stylesheets/theme'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import App from './App.vue'
 import router from './router'
+import '@mdi/font/css/materialdesignicons.css'
+import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(
+  createVuetify({
+    ...vuetifyConfig,
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
+  }),
+)
 
 app.mount('#app')
---
+++ src/stylesheets/theme.ts
@@ -0,0 +1,77 @@
+export const vuetifyConfig = {
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          // White
+          background: '#FFFFFF',
+          surface: '#FCFCFC',
+          // Gray
+          primary: '#191919', // Gray-9
+          secondary: '#666666', //Gray-6
+          info: '#999999', //Gray-4
+          // Green
+          accent: '#2C742F', // Dark
+          success: '#17b71d', // Light
+          // Orange
+          warning: '#FF8A00',
+          // Red
+          error: '#EA4B48',
+        },
+      },
+    },
+  },
+  defaults: {
+    global: {
+      style: `font-family: '"Roboto", "Noto Sans TC", sans-serif'`,
+    },
+    // Button
+    VBtn: {
+      block: false,
+      variant: 'flat',
+      rounded: 'xl',
+      style: `
+        font-size: 1rem;
+        font-weight: 500;
+        text-transform: none;
+      `,
+    },
+    // Card
+    VCard: {
+      rounded: 'lg',
+      color: 'surface',
+      elevation: 0,
+    },
+    VCardTitle: {
+      class: 'text-primary',
+      style: 'padding: 0;',
+    },
+    VCardText: {
+      class: 'text-body-1 text-secondary',
+      style: 'padding: 0;',
+    },
+    VCardActions: {
+      VBtn: {
+        variant: 'flat',
+        style: `padding: 0;
+        text-transform: none; `,
+      },
+    },
+    // Menu list item
+    VListItem: {
+      color: 'info',
+      style: `
+        font-size: 1rem;
+        font-weight: 400;
+      `,
+    },
+    VListItemTitle: {
+      style: `
+        font-size: 1rem;
+        font-weight: 400;
+      `,
+    },
+  },
+}
