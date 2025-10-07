5105e1494aea365a2826584e6680565f81d1f1cf
shu-ya318 <funghi0983524367@gmail.com>
2025-10-07 22:47:30 +0800

    fix(user) update OAuth2 redirect URL for backend container deployment

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -60,30 +60,57 @@ const onLogin = handleSubmit(async (values) => {
 
 const onLoginWithGoogle = () => {
   isLoading.value = true
-  window.location.href = `http://localhost:8080/oauth2/authorization/google`
+  window.location.href = `http://localhost:8080/onlineShop/oauth2/authorization/google`
 }
 </script>
 
 <template>
   <!-- Form -->
-  <auth-form-card title="Login" button-text="Login with email" :loading="isSubmitting" :disabled="isSubmitting"
-    @submit="onLogin">
+  <auth-form-card
+    title="Login"
+    button-text="Login with email"
+    :loading="isSubmitting"
+    :disabled="isSubmitting"
+    @submit="onLogin"
+  >
     <template #form-inputs>
       <!-- Inputs -->
-      <form-input label="Email" v-model="email" :required="true" :error-messages="errors.email"></form-input>
-      <form-input label="Password" v-model="password" :type="isPasswordVisible ? 'text' : 'password'" :required="true"
-        :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'" :error-messages="errors.password"
-        @click:append-inner="isPasswordVisible = !isPasswordVisible"></form-input>
+      <form-input
+        label="Email"
+        v-model="email"
+        :required="true"
+        :error-messages="errors.email"
+      ></form-input>
+      <form-input
+        label="Password"
+        v-model="password"
+        :type="isPasswordVisible ? 'text' : 'password'"
+        :required="true"
+        :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+        :error-messages="errors.password"
+        @click:append-inner="isPasswordVisible = !isPasswordVisible"
+      ></form-input>
     </template>
     <!-- Login with Google -->
     <template #third-party-auths>
       <v-divider color="info" :thickness="2">or</v-divider>
-      <v-btn prepend-icon="mdi-google" color="accent" class="mb-4" :block="true" :loading="isLoading"
-        :disabled="isLoading" @click="onLoginWithGoogle">Login with Google</v-btn>
+      <v-btn
+        prepend-icon="mdi-google"
+        color="accent"
+        class="mb-4"
+        :block="true"
+        :loading="isLoading"
+        :disabled="isLoading"
+        @click="onLoginWithGoogle"
+        >Login with Google</v-btn
+      >
     </template>
     <!-- Redirect link -->
     <template #redirect-link>
-      <router-link class="text-decoration-none text-primary bg-transparent" :to="{ name: 'register' }">
+      <router-link
+        class="text-decoration-none text-primary bg-transparent"
+        :to="{ name: 'register' }"
+      >
         Don't have account?
         <span style="color: #000000; font-weight: bold">Register</span>
       </router-link>
